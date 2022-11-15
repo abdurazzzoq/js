365b0fd8925f100cb4ce79b9246b3ab30877677d
abdurazzzoq <salemialekum34@gmail.com>
2022-11-15 18:58:50 +0500

    js

---
+++ script.js
@@ -0,0 +1,7 @@
+let job = prompt("What is your job?");
+let birthPlace = prompt("Where you was born?");
+let school = prompt("What school did you graduate from?");
+let currentJob = prompt("Where are you work?");
+let educationCenter = prompt("Where did you studied?");
+
+console.log (`I am ${job}. I was born in ${birthPlace}. I graduated ${school}school. I work as a developer in ${currentJob} company. I studied in ${educationCenter}.`);   
